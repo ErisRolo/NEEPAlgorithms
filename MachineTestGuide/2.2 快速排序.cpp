b201c5bccec84e67aef0d16ba12fe1846f879565
guohouxiao <122492508@qq.com>
2018-02-06 11:22:18 +0800

    QuickSort

---
+++ MachineTestGuide/2.2 快速排序.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    int buf[10000];
+    while(scanf("%d", &n) != EOF) {
+        for(int i = 0; i < n; i++) {
+            scanf("%d", &buf[i]);
+        }
+        sort(buf, buf + n); //使用该重载形式，表明将要使用自己定义的排列规则
+        for(int i = 0; i < n; i++) {
+            printf("%d ", buf[i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
