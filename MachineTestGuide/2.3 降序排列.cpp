fde0e317d590cfc06b5677224229f74adfe53418
guohouxiao <122492508@qq.com>
2018-02-06 11:29:30 +0800

    DescendingOrder

---
+++ MachineTestGuide/2.3 降序排列.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+//定义排序规则
+bool cmp(int x, int y) {
+    return x > y;
+}
+
+int main() {
+    int n;
+    int buf[100];
+    while(scanf("%d", &n) != EOF) {
+        for(int i = 0; i < n; i++) {
+            scanf("%d", &buf[i]);
+        }
+        sort(buf, buf + n, cmp); //使用该重载形式，表明将要使用自己定义的排列规则
+        for(int i = 0; i < n; i++) {
+            printf("%d ", buf[i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
