50bec52133aff5f9392b8e65416c4778e3af9af3
guohouxiao <122492508@qq.com>
2018-03-13 23:47:23 +0800

    UpdateBubble

--- MachineTestGuide/2.1 冒泡排序.cpp
+++ MachineTestGuide/2.1 冒泡排序.cpp
@@ -8,7 +8,7 @@ int main() {
             scanf("%d", &buf[i]); //输入待排序数字
         }
         //冒泡排序主体
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n - 1; i++) {
             for(int j = 0; j < n - 1 - i; j++) {
                 if(buf[j] > buf[j + 1]) {
                     int tmp = buf[j];
